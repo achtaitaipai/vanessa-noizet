5ba38f5c2a603ea567e6ecc71d63c69c670cc332
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-17 11:40:24 +0200

    feat: articles collection

---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { glob } from 'astro/loaders'
+import { z, defineCollection } from 'astro:content'
+
+export const collections = {
+	articles: defineCollection({
+		loader: glob({ pattern: '**/*.md', base: 'data/articles' }),
+		schema: z.object({
+			title: z.string(),
+			date: z.date()
+		})
+	})
+}
