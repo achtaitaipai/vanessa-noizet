beefe0bb202bf2f25a28552f9eb266bc2e9d5b25
achtaitaipai <charles.cailleteau@gmail.com>
2025-06-17 12:31:12 +0200

    feat: astro-typesafe-routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import typesafeRoutes from 'astro-typesafe-routes';
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [typesafeRoutes()]
+})
